5eed1a6a67a0e03b40713488b7cb8b5ecdb9e883
xarlys32 <57732203+xarlys32@users.noreply.github.com>
2020-09-02 21:59:43 +0200

    aaa

--- my-first-app/src/app/animation/animation-trigger/animation-trigger.component.ts
+++ my-first-app/src/app/animation/animation-trigger/animation-trigger.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate} from '@angular/animations'
+import { trigger, state, style, transition, animate, keyframes} from '@angular/animations'
 
 @Component({
   selector: 'app-animation-trigger',
@@ -20,27 +20,67 @@ import { trigger, state, style, transition, animate} from '@angular/animations'
       'transform': 'translateX(100px) scale(1)'})),
       state('shrunken', style({'background': 'orange',
         'transform': 'translateX(0) scale(0.5)'})),
-      transition('normal <=> highlighted', animate(300)),
-      /*transition('shrunken <=> *', [
+      transition('normal <=> highlighted', [
         style({
           'background': 'cyan'
         }),
-        animate(1000, style ({
-          'border-radius': '50px'
-        })),
-        animate(300)
-      ])*/
+        animate(1000)]),
+      
       transition('shrunken <=> *', animate(1000, style ({
           'border-radius': '50px'
         })))
-        
-    ])
-  ]
+    ]),
+    trigger('list1',[
+      state('in', style({
+        'opacity': 1,
+        'transform': 'translateX(0)'
+      })),
+      transition('void=>*', [
+        style({
+          'opacity': 0,
+          'transform': 'translateX(-100px)'
+        }),
+        animate(300)
+      ])
+    ]),
+    trigger('list2',[
+      state('in', style({
+        'opacity': 1,
+        'transform': 'translateX(0)'
+      })),
+      transition('void=>*', [
+        animate(1000, keyframes([
+          style({
+            'transform': 'translateX(-100px)',
+            'opacity': 0,
+            'offset': 0
+          }),
+          style({
+            'transform': 'translateX(-50px)',
+            'opacity': 0.5,
+            'offset': 0.3
+          }),
+          style({
+            'transform': 'translateX(-20px)',
+            'opacity': 1,
+            'offset': 0.8
+          }),
+          style({
+            'transform': 'translateX(0px)',
+            'opacity': 1,
+            'offset': 1
+          }),
+        ]))
+      ])
+  ])
+]
 })
 export class AnimationTriggerComponent implements OnInit {
 
   state: string ='normal'
   wildState:string = 'normal'
+  list: string[] = ['lickeriono','nemesio']
+  cosa: string
   constructor() { }
 
   ngOnInit() {
@@ -54,6 +94,14 @@ export class AnimationTriggerComponent implements OnInit {
 
   onWild() {
     this.wildState = 'shrunken' == this.wildState ? 'normal' : 'shrunken'
+  }
+
+  onAdd() {
+    this.list.push(this.cosa)
+  }
+
+  onAnimationStart(event) {
+    console.log(event)
   }
 
 }
