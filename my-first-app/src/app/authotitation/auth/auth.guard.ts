93a296a66192fc9c46e682c2fe3b629a0651d7cb
xarlys32 <57732203+xarlys32@users.noreply.github.com>
2020-09-29 23:48:41 +0200

    aa

--- my-first-app/src/app/authotitation/auth/auth.guard.ts
+++ my-first-app/src/app/authotitation/auth/auth.guard.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+
+    constructor(authService: AuthService) {
+        
+    }
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
         throw new Error('Method not implemented.');
     }
